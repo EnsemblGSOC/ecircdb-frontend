487b3aca9ca5c0c749a02c128d8c0b99c62fcdec
pradumangoyal <goyal.praduman611@gmail.com>
2019-07-30 00:02:12 +0530

    Modify export list feilds and filters

--- src/components/sampleList/exportList.js
+++ src/components/sampleList/exportList.js
@@ -11,11 +11,8 @@ export default class ExportList extends React.Component {
 
   componentDidMount() {
     this.setState({
-      chromosome: [],
-      classification: [],
       tpm: 0,
       nMethods: 0,
-      size: 0,
       format: 'csv',
       downloading: false
     })
@@ -31,22 +28,12 @@ export default class ExportList extends React.Component {
         downloading: true
       })
       const { speciesId, assemblyId, sampleId } = this.props
-      const {
-        chromosome,
-        classification,
-        tpm,
-        nMethods,
-        size,
-        format
-      } = this.state
+      const { tpm, nMethods, format } = this.state
       axios
         .get(urlExportSampleView(speciesId, assemblyId, sampleId), {
           params: {
-            chromosome,
-            classification,
             tpm,
             nMethods,
-            size,
             format
           }
         })
@@ -70,101 +57,47 @@ export default class ExportList extends React.Component {
   }
 
   render() {
-    const { data } = this.props
-
     return (
       <div className="export-list-wrapper">
         <Header as="h4">Export List:</Header>
-        <div className="export-field">
-          <strong className="export-field-heading">Chromosomes: </strong>
-          <Dropdown
-            placeholder="All chromosomes"
-            multiple
-            fluid
-            selection
-            search
-            clearable
-            value={this.state.chromosome}
-            onChange={this.handleChange}
-            name="chromosome"
-            options={data.distinctChromosomes.map(chromosome => {
-              return {
-                key: chromosome,
-                text: chromosome,
-                value: chromosome
-              }
-            })}
-          />
-        </div>
-        <div className="export-field">
-          <strong className="export-field-heading">Classifications: </strong>
-          <Dropdown
-            placeholder="All classifications"
-            multiple
-            fluid
-            selection
-            search
-            clearable
-            value={this.state.classification}
-            onChange={this.handleChange}
-            name="classification"
-            options={data.distinctClassifications.map(classification => {
-              return {
-                key: classification,
-                text: classification,
-                value: classification
-              }
-            })}
-          />
-        </div>
-        <div className="export-field">
-          <strong className="export-field-heading">Minimum TPM: </strong>
-          <Form.Input
-            name="tpm"
-            placeholder="Minimum TPM"
-            type="number"
-            min={0}
-            value={this.state.tpm}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="export-field">
-          <strong className="export-field-heading">Minimum N Methods: </strong>
-          <Form.Input
-            name="nMethods"
-            placeholder="Minimum N Methods"
-            type="number"
-            min={0}
-            value={this.state.nMethods}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="export-field">
-          <strong className="export-field-heading">Minimum Size: </strong>
-          <Form.Input
-            name="size"
-            placeholder="Minimum Size"
-            type="number"
-            min={0}
-            value={this.state.size}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="export-field">
-          <strong className="export-field-heading">Export Format: </strong>
-          <Dropdown
-            placeholder="Required format"
-            selection
-            search
-            value={this.state.format}
-            onChange={this.handleChange}
-            name="format"
-            options={[
-              { text: 'CSV', value: 'csv' },
-              { text: 'FASTA', value: 'fasta' }
-            ]}
-          />
-        </div>
+        <Form>
+          <Form.Group>
+            <Form.Input
+              label="Minimum TPM:"
+              name="tpm"
+              placeholder="Minimum TPM"
+              type="number"
+              step="any"
+              min={0}
+              value={this.state.tpm}
+              onChange={this.handleChange}
+            />
+            <Form.Input
+              label="Minimum N Methods:"
+              name="nMethods"
+              placeholder="Minimum N Methods"
+              type="number"
+              step="any"
+              min={0}
+              value={this.state.nMethods}
+              onChange={this.handleChange}
+            />
+            <Form.Dropdown
+              as={Form.Field}
+              label="Export Format:"
+              placeholder="Required format"
+              selection
+              search
+              value={this.state.format}
+              onChange={this.handleChange}
+              name="format"
+              options={[
+                { text: 'CSV', value: 'csv' },
+                { text: 'FASTA', value: 'fasta' }
+              ]}
+            />
+          </Form.Group>
+        </Form>
         <Button className="sample-details-button" onClick={this.handleClick}>
           <Icon name="file download" />
           Download
--- src/components/speciesView/exportList.js
+++ src/components/speciesView/exportList.js
@@ -11,12 +11,9 @@ export default class ExportList extends React.Component {
 
   componentDidMount() {
     this.setState({
-      chromosome: [],
-      classification: [],
       tissue: [],
       tpm: 0,
       nMethods: 0,
-      size: 0,
       format: 'csv',
       downloading: false
     })
@@ -32,24 +29,13 @@ export default class ExportList extends React.Component {
         downloading: true
       })
       const { speciesId, assemblyId } = this.props
-      const {
-        chromosome,
-        classification,
-        tissue,
-        tpm,
-        nMethods,
-        size,
-        format
-      } = this.state
+      const { tissue, tpm, nMethods, format } = this.state
       axios
         .get(urlExportSpeciesView(speciesId, assemblyId), {
           params: {
-            chromosome,
-            classification,
             tissue,
             tpm,
             nMethods,
-            size,
             format
           }
         })
@@ -82,51 +68,10 @@ export default class ExportList extends React.Component {
           Export List
         </strong>
         <div className="sample-details-divider" />
-        <div className="export-field">
-          <strong className="export-field-heading">Chromosomes: </strong>
-          <Dropdown
-            placeholder="All chromosomes"
-            multiple
-            fluid
-            selection
-            search
-            clearable
-            value={this.state.chromosome}
-            onChange={this.handleChange}
-            name="chromosome"
-            options={data.distinctChromosomes.map(chromosome => {
-              return {
-                key: chromosome,
-                text: chromosome,
-                value: chromosome
-              }
-            })}
-          />
-        </div>
-        <div className="export-field">
-          <strong className="export-field-heading">Classifications: </strong>
-          <Dropdown
-            placeholder="All classifications"
-            multiple
-            fluid
-            selection
-            search
-            clearable
-            value={this.state.classification}
-            onChange={this.handleChange}
-            name="classification"
-            options={data.distinctClassifications.map(classification => {
-              return {
-                key: classification,
-                text: classification,
-                value: classification
-              }
-            })}
-          />
-        </div>
-        <div className="export-field">
-          <strong className="export-field-heading">Tissues: </strong>
-          <Dropdown
+        <Form>
+          <Form.Dropdown
+            as={Form.Field}
+            label="Tissues"
             placeholder="All tissues"
             multiple
             fluid
@@ -144,55 +89,43 @@ export default class ExportList extends React.Component {
               }
             })}
           />
-        </div>
-        <div className="export-field">
-          <strong className="export-field-heading">Minimum TPM: </strong>
-          <Form.Input
-            name="tpm"
-            placeholder="Minimum TPM"
-            type="number"
-            min={0}
-            value={this.state.tpm}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="export-field">
-          <strong className="export-field-heading">Minimum N Methods: </strong>
-          <Form.Input
-            name="nMethods"
-            placeholder="Minimum N Methods"
-            type="number"
-            min={0}
-            value={this.state.nMethods}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="export-field">
-          <strong className="export-field-heading">Minimum Size: </strong>
-          <Form.Input
-            name="size"
-            placeholder="Minimum Size"
-            type="number"
-            min={0}
-            value={this.state.size}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="export-field">
-          <strong className="export-field-heading">Export Format: </strong>
-          <Dropdown
-            placeholder="Required format"
-            selection
-            search
-            value={this.state.format}
-            onChange={this.handleChange}
-            name="format"
-            options={[
-              { text: 'CSV', value: 'csv' },
-              { text: 'FASTA', value: 'fasta' }
-            ]}
-          />
-        </div>
+          <Form.Group>
+            <Form.Input
+              name="tpm"
+              label="Minimum TPM:"
+              placeholder="Minimum TPM"
+              type="number"
+              step="any"
+              min={0}
+              value={this.state.tpm}
+              onChange={this.handleChange}
+            />
+            <Form.Input
+              name="nMethods"
+              label="Minimum N Methods:"
+              placeholder="Minimum N Methods"
+              type="number"
+              step="any"
+              min={0}
+              value={this.state.nMethods}
+              onChange={this.handleChange}
+            />
+            <Form.Dropdown
+              as={Form.Field}
+              label="Export Format:"
+              placeholder="Required format"
+              selection
+              search
+              value={this.state.format}
+              onChange={this.handleChange}
+              name="format"
+              options={[
+                { text: 'CSV', value: 'csv' },
+                { text: 'FASTA', value: 'fasta' }
+              ]}
+            />
+          </Form.Group>
+        </Form>
         <Button className="sample-details-button" onClick={this.handleClick}>
           <Icon name="file download" />
           Download
