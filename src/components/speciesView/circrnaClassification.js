334e3afe1b1b5c0da46fa6def98694cf77220f56
pradumangoyal <goyal.praduman611@gmail.com>
2019-08-07 02:00:20 +0530

    Add pie chart for circrna classifications

---
+++ src/components/speciesView/circrnaClassification.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Card } from 'semantic-ui-react'
+import Plot from 'react-plotly.js'
+
+import './css/graphs.css'
+
+export default class Graph extends React.Component {
+  render() {
+    var data = [
+      {
+        values: this.props.values,
+        labels: this.props.labels,
+        domain: { column: 0 },
+        name: 'circRNA classification',
+        hoverinfo: 'label+percent+name',
+        hole: 0.4,
+        type: 'pie'
+      }
+    ]
+
+    var layout = {
+      title: 'circRNA classification',
+      grid: { rows: 1, columns: 1 }
+    }
+
+    return (
+      <Card className="highlight-card">
+        <div>
+          <Plot
+            data={data}
+            layout={layout}
+            responsive
+            className="graph-wrapper"
+          />
+        </div>
+        <div className="graph-legend">
+          <span className="graph-heading">circRNA classificaion</span>
+          <span className="graph-description">(description)</span>
+        </div>
+      </Card>
+    )
+  }
+}
