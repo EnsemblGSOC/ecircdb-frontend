129b204fc30b217da5121edd88c63a1deb56778b
pradumangoyal <goyal.praduman611@gmail.com>
2019-08-07 02:01:52 +0530

    Link all of the ready plots to the speciesView

--- src/components/speciesView/graphs.js
+++ src/components/speciesView/graphs.js
@@ -2,10 +2,17 @@ import React from 'react'
 import { Card, Icon } from 'semantic-ui-react'
 
 import CircRNAPerLocus from './circRNAPerLocus'
+import CircrnaPerChromosome from './circRNAPerChromosome'
 import CircRNAVSLTPerLocus from './ncrnaVsNltPerLocus'
+import CircRNANExons from './circrnaNExons'
 import NumberOfExons from './numberOfExons'
 import SizeOfExons from './sizeOfExons'
+import CircrnaClassification from './circrnaClassification'
+import CircrnaNMethods from './circrnaNMethods'
 import AverageTPM from './averageTpm'
+import AverageJPM from './averageJpm'
+import AverageAR from './averageAr'
+import CircrnaSize from './circrnaSize'
 
 import './css/graphs.css'
 
@@ -16,6 +23,16 @@ export default class Graphs extends React.Component {
       type: 'box',
       boxmean: 'sd',
       boxpoints: 'Outliers'
+    }
+    var sizeTrace = {
+      type: 'violin',
+      points: 'none',
+      box: {
+        visible: true
+      },
+      meanline: {
+        visible: true
+      }
     }
     return (
       <div className="graphs-container">
@@ -34,12 +51,25 @@ export default class Graphs extends React.Component {
             x={data.circRNAPerLocus.locusId}
             y={data.circRNAPerLocus.count}
           />
+          <CircrnaPerChromosome
+            x={data.chromosomesCircrnaCount.chromosomes}
+            y={data.chromosomesCircrnaCount.circrnas}
+          />
           <CircRNAVSLTPerLocus
             x={data.circrnaVsLtPerLocus.countCj}
             y={data.circrnaVsLtPerLocus.countBj}
             text={data.circrnaVsLtPerLocus.text}
             size={data.circrnaVsLtPerLocus.nexons}
           />
+          <CircrnaClassification
+            values={data.circrnaClassification.values}
+            labels={data.circrnaClassification.labels}
+          />
+          <CircrnaNMethods
+            values={data.circrnaNMethods.values}
+            labels={data.circrnaNMethods.labels}
+          />
+          <CircRNANExons values={data.circrnaNExons} />
           <AverageTPM
             data={data.tpmTissueBoxplot.tissueList.map(tissue => {
               return {
@@ -48,6 +78,44 @@ export default class Graphs extends React.Component {
                 name: tissue
               }
             })}
+          />
+          <AverageJPM
+            data={data.jpmTissueBoxplot.tissueList.map(tissue => {
+              return {
+                ...trace,
+                y: data.jpmTissueBoxplot.jpmGrouped[tissue],
+                name: tissue
+              }
+            })}
+          />
+          <AverageAR
+            data={data.arTissueBoxplot.tissueList.map(tissue => {
+              return {
+                ...trace,
+                y: data.arTissueBoxplot.arGrouped[tissue],
+                name: tissue
+              }
+            })}
+          />
+          <CircrnaSize
+            data={[
+              {
+                ...sizeTrace,
+                x: data.sizeTissueBoxplot.tissueList,
+                y: data.sizeTissueBoxplot.genomicSizes,
+                legendgroup: 'Genomic size',
+                scalegroup: 'Genomic size',
+                name: 'Genomic size'
+              },
+              {
+                ...sizeTrace,
+                x: data.sizeTissueBoxplot.tissueList,
+                y: data.sizeTissueBoxplot.splicedSizes,
+                legendgroup: 'Spliced size',
+                scalegroup: 'Spliced size',
+                name: 'Spliced size'
+              }
+            ]}
           />
           {/* <NumberOfExons />
           <SizeOfExons />
