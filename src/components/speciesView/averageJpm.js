ac66ba716d403498d515d61cf72682038df1ed4d
pradumangoyal <goyal.praduman611@gmail.com>
2019-08-07 01:57:16 +0530

    Add boxplot for averageJpm

---
+++ src/components/speciesView/averageJpm.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Card } from 'semantic-ui-react'
+import Plot from 'react-plotly.js'
+
+import './css/graphs.css'
+
+export default class Graph extends React.Component {
+  render() {
+    var layout = {
+      title: 'Average circRNA expression (JPM)',
+      font: {
+        family: "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif",
+        size: 14
+      },
+      xaxis: {
+        title: 'Tissues'
+      },
+      yaxis: {
+        title: 'z-score(Log2 expression estimate)'
+      }
+    }
+
+    return (
+      <Card className="highlight-card">
+        <div>
+          <Plot
+            data={this.props.data}
+            layout={layout}
+            responsive
+            className="graph-wrapper"
+          />
+        </div>
+        <div className="graph-legend">
+          <span className="graph-heading">
+            Average circRNA expression (JPM)
+          </span>
+          <span className="graph-description">(description)</span>
+        </div>
+      </Card>
+    )
+  }
+}
